bf4a0c2eb8682234df81fdd501b7e7a833ee2f31
Angie <angieamado76@gmail.com>
2026-01-20 12:22:37 +0100

    feat: agrega getters, setters y constructores manualmente sin lombok

--- src/main/java/com/midominio/group/app/spring/entity/Pregunta.java
+++ src/main/java/com/midominio/group/app/spring/entity/Pregunta.java
@@ -1,23 +1,17 @@
 package com.midominio.group.app.spring.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@NoArgsConstructor //por lo que siempre se necesita un constructor vacío
-@AllArgsConstructor //útil para crear objeto con todos los datos
 @Entity
 @Table(name = "preguntas")
 @Inheritance(strategy = InheritanceType.JOINED) //permite crear tablas separadas
 @DiscriminatorColumn(name = "tipo_pregunta", discriminatorType = DiscriminatorType.STRING)
 public abstract class Pregunta {
     
+	//1. ATRIBUTOS
+	
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -39,9 +33,76 @@ public abstract class Pregunta {
         fechaCreacion = LocalDateTime.now();
     }
     
-    // Método abstracto que cada tipo de pregunta implementará
+    
+    //2. CONSTRUCTORES
+    
+    public Pregunta () {
+    	
+    }
+    
+    public Pregunta(Long id, String enunciado, String tematica, LocalDateTime fechaCreacion, Boolean activa) {
+		this.id = id;
+		this.enunciado = enunciado;
+		this.tematica = tematica;
+		this.fechaCreacion = fechaCreacion;
+		this.activa = activa;
+	}
+
+    
+    
+    //3. METODOS
+	// Método abstracto que cada tipo de pregunta implementará
     public abstract String getTipoPregunta();
     
+    
+    
+
+
+	//4. GETTERS Y SETTERS
+    
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEnunciado() {
+		return enunciado;
+	}
+
+	public void setEnunciado(String enunciado) {
+		this.enunciado = enunciado;
+	}
+
+	public String getTematica() {
+		return tematica;
+	}
+
+	public void setTematica(String tematica) {
+		this.tematica = tematica;
+	}
+
+	public LocalDateTime getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(LocalDateTime fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public Boolean getActiva() {
+		return activa;
+	}
+
+	public void setActiva(Boolean activa) {
+		this.activa = activa;
+	}
+
+
+    
+    //5. HASHCODE y EQUALS
     //Para mantener la persistencia en el mapeo de datos 
     @Override
     public boolean equals(Object o) {
