1e3f0179590ede1da72180fe2082ee405a62f49e
Angie Amado <angieamado76@gmail.com>
2026-01-24 18:12:15 +0100

    feat: agrega PreguntaVF los getters y setters

--- src/main/java/com/midominio/group/app/spring/entity/PreguntaVerdaderoFalso.java
+++ src/main/java/com/midominio/group/app/spring/entity/PreguntaVerdaderoFalso.java
@@ -1,25 +1,32 @@
 package com.midominio.group.app.spring.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "preguntas_verdadero_falso")
 @DiscriminatorValue("VERDADERO_FALSO")
 public class PreguntaVerdaderoFalso extends Pregunta {
     
+	//1. ATRIBUTOS
     @Column(name = "respuesta_correcta", nullable = false) //este campo no puede estar vacío
     private Boolean respuestaCorrecta;
     
     @Column(length = 1000)
     private String explicacion;
+    
+    
+    //CONSTRUCTORES
+    
+    public PreguntaVerdaderoFalso() {
+    	
+    }
+    
+    public PreguntaVerdaderoFalso(Boolean respuestaCorrecta, String explicacion) {
+		this.respuestaCorrecta = respuestaCorrecta;
+		this.explicacion = explicacion;
+	}
+    
+    //METODOS
     
     @Override
     public String getTipoPregunta() {
@@ -29,4 +36,24 @@ public class PreguntaVerdaderoFalso extends Pregunta {
     public boolean validarRespuesta(Boolean respuesta) {
         return respuesta != null && respuesta.equals(this.respuestaCorrecta);
     }
+
+
+	//GETTERS Y SETTERS
+	public Boolean getRespuestaCorrecta() {
+		return respuestaCorrecta;
+	}
+
+	public void setRespuestaCorrecta(Boolean respuestaCorrecta) {
+		this.respuestaCorrecta = respuestaCorrecta;
+	}
+
+	public String getExplicacion() {
+		return explicacion;
+	}
+
+	public void setExplicacion(String explicacion) {
+		this.explicacion = explicacion;
+	}
+    
+    
 }
