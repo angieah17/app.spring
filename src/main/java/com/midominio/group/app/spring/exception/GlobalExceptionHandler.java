c0f87a0184865c6384f282895bbebc528597eea0
Angie Amado <angieamado76@gmail.com>
2026-01-26 20:16:43 +0100

    feat: agregaGlobalExceptionHandler con 4 métodos handler

---
+++ src/main/java/com/midominio/group/app/spring/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.midominio.group.app.spring.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+}
