c113706d56b9a4d20d3e30a77430b05e32981158
Angie <angieamado76@gmail.com>
2026-02-05 11:05:33 +0100

    feat: agrega configuración para que funcione el CORS

--- src/main/java/com/midominio/group/app/spring/config/SecurityConfig.java
+++ src/main/java/com/midominio/group/app/spring/config/SecurityConfig.java
@@ -1,20 +1,44 @@
 package com.midominio.group.app.spring.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
+	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable()) // Deshabilita CSRF (solo pruebas)
+            .csrf(csrf -> csrf.disable())
+            .cors(cors -> {})   // 👈 CLAVE
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll() // Permite todas las rutas
+                .anyRequest().permitAll()
             );
+
         return http.build();
     }
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+	
 }
