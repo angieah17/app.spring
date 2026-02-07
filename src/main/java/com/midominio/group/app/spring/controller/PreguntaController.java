2f5878c56b5e53fb5f30bac005e6eee1c1aa5104
Angie Amado <angieamado76@gmail.com>
2026-02-07 20:34:14 +0100

    feat: agrega crossorigin para conectar con front

--- src/main/java/com/midominio/group/app/spring/controller/PreguntaController.java
+++ src/main/java/com/midominio/group/app/spring/controller/PreguntaController.java
@@ -1,5 +1,6 @@
 package com.midominio.group.app.spring.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import com.midominio.group.app.spring.service.PreguntaService;
 
 /* Controlador REST para operaciones sobre preguntas */
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/preguntas")
 public class PreguntaController {
