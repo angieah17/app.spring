7024ca6bfc0930451a3bb782c0962e3a7d860605
Angie Amado <angieamado76@gmail.com>
2026-02-08 20:21:39 +0100

    fix: cambia nombre y endpoints del controller para separar la logica del negocio

--- src/main/java/com/midominio/group/app/spring/controller/JuegoPreguntaController.java
+++ src/main/java/com/midominio/group/app/spring/controller/JuegoPreguntaController.java
@@ -9,38 +9,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.midominio.group.app.spring.entity.Pregunta;
-import com.midominio.group.app.spring.service.PreguntaService;
+import com.midominio.group.app.spring.service.JuegoPreguntaService;
 
 /* Controlador REST para operaciones sobre preguntas */
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
-@RequestMapping("/api/preguntas")
-public class PreguntaController {
+@RequestMapping("/api/juego/preguntas")
+public class JuegoPreguntaController {
 
-    private final PreguntaService preguntaService;
+    private final JuegoPreguntaService juegoPreguntaService;
 
-    public PreguntaController(PreguntaService preguntaService) {
-        this.preguntaService = preguntaService;
+    public JuegoPreguntaController(JuegoPreguntaService preguntaService) {
+        this.juegoPreguntaService = preguntaService;
     }
 
     @GetMapping("/random")
     public Pregunta obtenerPreguntaAleatoria() {
-        return preguntaService.obtenerPreguntaAleatoria();
+        return juegoPreguntaService.obtenerPreguntaAleatoria();
     }
-    
-    @GetMapping("/tematica/{tematica}")
-    public Page<Pregunta> obtenerPreguntasPorTematica(
-            @PathVariable String tematica,
-            Pageable pageable) {
-        return preguntaService.obtenerPreguntasPorTematica(tematica, pageable);
-    }
+
 
     @GetMapping("/tematica/{tematica}/activas")
 public Page<Pregunta> obtenerPreguntasActivasPorTematica(
         @PathVariable String tematica,
         Pageable pageable) {
-    return preguntaService.obtenerPreguntasActivasPorTematica(tematica, pageable);
+    return juegoPreguntaService.obtenerPreguntasActivasPorTematica(tematica, pageable);
 }
     
 }
