906c11286648ce289267faf6d9e5000e90294163
Angie Amado <angieamado76@gmail.com>
2026-02-11 17:15:03 +0100

    feat: agrega UserDetailsImpl para envolver al Usuario

---
+++ src/main/java/com/midominio/group/app/spring/config/UserDetailsImpl.java
@@ -0,0 +1,51 @@
+package com.midominio.group.app.spring.config;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.midominio.group.app.spring.entity.Usuario;
+
+public class UserDetailsImpl implements UserDetails {
+    
+
+	private static final long serialVersionUID = 1L;
+	
+	private final Usuario usuario;
+
+    public UserDetailsImpl(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(
+            new SimpleGrantedAuthority("ROLE_" + usuario.getRole().name())
+        );
+    }
+
+	@Override
+	public @Nullable String getPassword() {
+		return usuario.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return usuario.getPassword();
+	}
+    
+	 @Override
+	    public boolean isEnabled() {
+	        return usuario.isEnabled();
+	    }
+
+	    public boolean isAccountNonExpired() { return true; }
+	    public boolean isAccountNonLocked() { return true; }
+	    public boolean isCredentialsNonExpired() { return true; }
+
+
+}
--- src/main/java/com/midominio/group/app/spring/entity/Usuario.java
+++ src/main/java/com/midominio/group/app/spring/entity/Usuario.java
@@ -32,8 +32,7 @@ public class Usuario {
     @Column(nullable = false, length = 20)
     private RoleEnum role;
     
-    @Column(name = "activo")
-    private boolean activo = true;
+    private boolean enabled = true;
 
     
     
@@ -47,48 +46,48 @@ public class Usuario {
         this.password = password;
         this.role = role;
     }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public RoleEnum getRole() {
+		return role;
+	}
+
+	public void setRole(RoleEnum role) {
+		this.role = role;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
     
     
-    // 3. GETTERS Y SETTERS
+ 
     
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public RoleEnum getRole() {
-        return role;
-    }
-
-    public void setRole(RoleEnum role) {
-        this.role = role;
-    }
-
-    public boolean getActivo() {
-        return activo;
-    }
-
-    public void setActivo(boolean activo) {
-        this.activo = activo;
-    }
-
 }
