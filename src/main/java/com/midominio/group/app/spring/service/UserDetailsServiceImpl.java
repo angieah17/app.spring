73c8d9a352101d5f6ed8c5e0f152e3efd0e6d35d
Angie Amado <angieamado76@gmail.com>
2026-02-11 17:23:49 +0100

    feat: se agrega servicio para cargar cliente a partir de su username

---
+++ src/main/java/com/midominio/group/app/spring/service/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.midominio.group.app.spring.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.midominio.group.app.spring.config.UserDetailsImpl;
+import com.midominio.group.app.spring.entity.Usuario;
+import com.midominio.group.app.spring.repository.UsuarioRepository;
+
+/* Servicio para cargar al usuario por su nombre de usuario */
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UserDetailsServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        Usuario usuario = usuarioRepository.findByUsername(username)
+            .orElseThrow(() ->
+                new UsernameNotFoundException("Usuario no encontrado"));
+
+        return new UserDetailsImpl(usuario);
+    }
+}
+
