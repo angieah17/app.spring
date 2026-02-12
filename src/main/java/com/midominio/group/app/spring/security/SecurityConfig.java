64f1c97c0a86be76dbd24feab582409d40bfef28
Angie Amado <angieamado76@gmail.com>
2026-02-12 20:35:13 +0100

    feat: agrega endpoint autorizado a SecurityConfig para el registro

--- src/main/java/com/midominio/group/app/spring/security/SecurityConfig.java
+++ src/main/java/com/midominio/group/app/spring/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,7 +28,7 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable()) //pues es una API interna
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
                 .requestMatchers("/api/**").authenticated()
                 .anyRequest().authenticated()
