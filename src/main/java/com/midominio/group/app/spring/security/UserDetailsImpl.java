139695ab97a78384157631254043f388a7b9b711
Angie Amado <angieamado76@gmail.com>
2026-02-12 20:11:00 +0100

    fix: ajusta el gestusername del userdetailsimpl

--- src/main/java/com/midominio/group/app/spring/security/UserDetailsImpl.java
+++ src/main/java/com/midominio/group/app/spring/security/UserDetailsImpl.java
@@ -35,7 +35,7 @@ public class UserDetailsImpl implements UserDetails {
 
 	@Override
 	public String getUsername() {
-		return usuario.getPassword();
+		return usuario.getUsername();
 	}
     
 	 @Override
